d8852383bf5e2dcf507e669b160863e028a5e7be
yeeping50@hotmail.com <yeeping50@hotmail.com>
2024-03-11 03:41:11 +0000

    Edited view.ItemTemplate1 and settings

--- client_code/view/ItemTemplate1/__init__.py
+++ client_code/view/ItemTemplate1/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...Form1 import Form1
 
 class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
@@ -11,3 +12,11 @@ class ItemTemplate1(ItemTemplate1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert(
+      content=Form1(),
+      title="Update Article",
+      large=True
+    )
